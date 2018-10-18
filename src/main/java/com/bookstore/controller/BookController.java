9c47940e8d7932ec2917189967ba3d53ab33ae82
Ari Aaltonen <aaltonengithub@gmail.com>
2018-10-18 15:34:51 +0300

    rest needs work

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -1,11 +1,18 @@
 package com.bookstore.controller;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.controller.Book;
 
 @Controller
 public class BookController {
@@ -19,20 +26,31 @@ public class BookController {
 
 		return "index";
 	}
+	//show all books
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String bookList(Model model) {
 		model.addAttribute("booklist", bookrepository.findAll());
-		//model.addAttribute("booklist", bkrepo.findAll());
 		return "booklist";
 	}
+	// REST list books
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) bookrepository.findAll();
+	}
+	//find book by isbn
+	@RequestMapping(value="/book/{isbn]", method = RequestMethod.GET)
+		public @ResponseBody Book findBookRest(@PathVariable("isbn") String isbn) {
+			return bookrepository.findByIsbn(isbn);	
+	}
 	
+	//add a book GET
 	@RequestMapping(value = "/addbook", method = RequestMethod.GET)
 	public String addbook(Model model) {
 		model.addAttribute("book", new Book());
 		model.addAttribute("categories", catRepo.findAll());
 		return "addbook";
 	}
-	
+	//add a book POST
 	@RequestMapping(value = "/addbook", method = RequestMethod.POST)
 	public String save(Book book, Model model) {
 		bookrepository.save(book);
@@ -41,20 +59,20 @@ public class BookController {
 		model.addAttribute("categories", catRepo.findAll());
 		return "addbook";
 	}
-	
+	// delete book
 	@RequestMapping(value = "/delete/{isbn}", method = RequestMethod.GET)
-	public String deleteStudent(@PathVariable("isbn") String isbn, Model model) {
+	public String deleteBook(@PathVariable("isbn") String isbn, Model model) {
 		bookrepository.deleteByIsbn(isbn);
 		return "redirect:/booklist";
 	}
-	
+	// edit book
 	@RequestMapping(value = "/editbook/{isbn}")
 	public String addBook(@PathVariable("isbn") String isbn, Model model){
 		model.addAttribute("book", bookrepository.findByIsbn(isbn));
 		model.addAttribute("categories", catRepo.findAll());
 		return "editbook";
 	}
-	
+	// edit book POST
 	@RequestMapping(value = "/editbook", method = RequestMethod.POST)
 	public String editbook(Book book, Model model) {
 		bookrepository.save(book);
@@ -63,7 +81,7 @@ public class BookController {
 		model.addAttribute("categories", catRepo.findAll());
 		return "editbook";
 	}
-	
+	// edit book GET
 	@RequestMapping(value = "/editbook", method = RequestMethod.GET)
 	public String editbook(Model model) {
 		model.addAttribute("book", new Book());
